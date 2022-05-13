b246e83da8d49f9c45e9bb82359ed8c052163e4b
mariazuritaa <102199486+mariazuritaa@users.noreply.github.com>
2022-05-12 20:10:35 -0500

    Add files via upload

---
+++ parcial2_A01173175.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE parcial2_A01173175", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
